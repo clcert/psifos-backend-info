8aec4a33b78abbd7cd054147e5ec141165352f67
Tobal <jaramillo.cristobal.a@gmail.com>
2022-02-17 10:22:15 -0300

    route voters info

--- app/voters_views.py
+++ app/voters_views.py
@@ -25,3 +25,31 @@ def get_election_voters(election_uuid):
         voter = Voter(election_uuid, row[0], row[1], voter_id_hash, voter_type)
         voters.append(voter.__dict__)
     return Response(json.dumps(voters), mimetype='application/json')
+
+
+@app.route("/elections/<election_uuid>/voters/info")
+def get_voters_info(election_uuid):
+
+    st_1 = text('select id from helios_election where uuid = \"%s\"' % election_uuid)
+    res_1 = db.engine.execute(st_1)
+    election_id = int([row[0] for row in res_1][0])
+
+    st_2 = text('select voter_login_id, voter_name, voter_email, vote_hash, voter_weight, uuid from helios_voter where election_id = \"%s\"' % election_id)
+    res_2 = db.engine.execute(st_2)
+    voters = []
+    votes_cast = 0
+    total_voters = 0
+    data = {}
+
+    for row in res_2:
+        voter = {'voter_login_id': row[0], 'name': row[1], 'voter_email': row[2], 'vote_hash': row[3], 'voter_weight': row[4], 'uuid': row[5]}
+        voters.append(voter)
+        if row[3]:
+            votes_cast += 1
+        total_voters += 1
+    
+    data["info_votes"] = {"votes_cast": votes_cast, "total_voters": total_voters}
+    data["info_voters"] = voters
+
+
+    return Response(json.dumps(data), mimetype='application/json')
