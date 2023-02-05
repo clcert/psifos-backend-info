76533d77f6f078bf9b100d0ca2ce3751e911ba60
jaramillo-cris <cristobal.jaramillo@destacame.cl>
2023-02-05 16:43:15 -0300

    delete blank vote in election model

--- app/psifos/model/schemas.py
+++ app/psifos/model/schemas.py
@@ -141,7 +141,6 @@ class ElectionBase(BaseModel):
     randomize_answer_order: bool | None
     private_p: bool | None
     normalization: bool | None
-    include_blank_null_vote: bool | None
 
 
 
